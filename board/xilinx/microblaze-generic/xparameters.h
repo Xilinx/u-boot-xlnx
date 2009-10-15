a797d4535d0f6ee6355ddd4c9ab3477d1ed0d69c
John Linn <john.linn@xilinx.com>
2009-10-15 15:24:56 -0600

    u-boot: microblaze default system definition
    Updated the system to match the ML507 system with LL TEMAC with DMA
    and the UART 16550 that the Linux kernel is using.
    
    Signed-off-by: John Linn <john.linn@xilinx.com>

--- board/xilinx/microblaze-generic/xparameters.h
+++ board/xilinx/microblaze-generic/xparameters.h
@@ -25,47 +25,31 @@
  * Version: Xilinx EDK 8.2.02 EDK_Im_Sp2.4
  */
 
-#define XILINX_BOARD_NAME	microblaze-generic
+#define XILINX_BOARD_NAME	microblaze-generic-lltemac-uart550
 
 /* System Clock Frequency */
 #define XILINX_CLOCK_FREQ	100000000
 
 /* Microblaze is microblaze_0 */
 #define XILINX_USE_MSR_INSTR	1
-#define XILINX_FSL_NUMBER	3
+#define XILINX_FSL_NUMBER	0
 
 /* Interrupt controller is opb_intc_0 */
-#define XILINX_INTC_BASEADDR	0x41200000
-#define XILINX_INTC_NUM_INTR_INPUTS	6
+#define XILINX_INTC_BASEADDR	0x81800000
+#define XILINX_INTC_NUM_INTR_INPUTS	7
 
 /* Timer pheriphery is opb_timer_1 */
-#define XILINX_TIMER_BASEADDR	0x41c00000
-#define XILINX_TIMER_IRQ	0
+#define XILINX_TIMER_BASEADDR	0x83C00000
+#define XILINX_TIMER_IRQ	4
 
 /* Uart pheriphery is RS232_Uart */
-#define XILINX_UARTLITE_BASEADDR	0x40600000
-#define XILINX_UARTLITE_BAUDRATE	115200
-
-/* IIC pheriphery is IIC_EEPROM */
-#define XILINX_IIC_0_BASEADDR	0x40800000
-#define XILINX_IIC_0_FREQ	100000
-#define XILINX_IIC_0_BIT	0
-
-/* GPIO is LEDs_4Bit*/
-#define XILINX_GPIO_BASEADDR	0x40000000
-
-/* Flash Memory is FLASH_2Mx32 */
-#define XILINX_FLASH_START	0x2c000000
-#define XILINX_FLASH_SIZE	0x00800000
+#define XILINX_UART16550_BASEADDR 0x83E00000
+#define XILINX_UART16550_CLOCK_HZ 100000000
 
 /* Main Memory is DDR_SDRAM_64Mx32 */
-#define XILINX_RAM_START	0x28000000
+#define XILINX_RAM_START	0x51000000
 #define XILINX_RAM_SIZE	0x04000000
 
-/* Sysace Controller is SysACE_CompactFlash */
-#define XILINX_SYSACE_BASEADDR	0x41800000
-#define XILINX_SYSACE_HIGHADDR	0x4180ffff
-#define XILINX_SYSACE_MEM_WIDTH	16
-
 /* Ethernet controller is Ethernet_MAC */
-#define XILINX_EMACLITE_BASEADDR       0x40C00000
+#define XILINX_LLTEMAC_BASEADDR 0x81C00000
+#define XILINX_LLTEMAC_SDMA_CTRL_BASEADDR 0x84600100
