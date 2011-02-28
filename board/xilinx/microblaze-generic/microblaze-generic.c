b9d1e8ba8f64a6127d0bef84086a75d36805efef
Michal Simek <monstr@monstr.eu>
2011-02-28 10:22:00 +0100

    microblaze: Fix no return statement from microblaze-generic board
    Remove compilation warning for systems without network support.
    
    Warning log:
    microblaze-generic.c: In function 'board_eth_init':
    microblaze-generic.c:82: warning: no return statement in function returning non-void
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- board/xilinx/microblaze-generic/microblaze-generic.c
+++ board/xilinx/microblaze-generic/microblaze-generic.c
@@ -71,12 +71,10 @@ int fsl_init2 (void) {
 
 int board_eth_init(bd_t *bis)
 {
-	/*
-	 * This board either has PCI NICs or uses the CPU's TSECs
-	 * pci_eth_init() will return 0 if no NICs found, so in that case
-	 * returning -1 will force cpu_eth_init() to be called.
-	 */
+	int ret = 0;
+
 #ifdef CONFIG_XILINX_EMACLITE
-	return xilinx_emaclite_initialize(bis, XILINX_EMACLITE_BASEADDR);
+	ret |= xilinx_emaclite_initialize(bis, XILINX_EMACLITE_BASEADDR);
 #endif
+	return ret;
 }
