c1fcbe2f776de87be78d27ddedf4c1a6574f363b
Denis Vasilik <contact@denisvasilik.com>
2016-12-09 23:10:16 +0100

    Added support for the ZYNQ Development Board - PYNQ.

---
+++ include/configs/zynq_pynq.h
@@ -0,0 +1,20 @@
+/*
+ * (C) Copyright 2012 Xilinx
+ * (C) Copyright 2014 Digilent Inc.
+ *
+ * Configuration for Zynq Development Board - PYNQ
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_PYNQ_H
+#define __CONFIG_ZYNQ_PYNQ_H
+
+/* PYNQ PS Clock Frequency 50MHz */
+#define CONFIG_ZYNQ_PS_CLK_FREQ	50000000UL
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_PYNQ_H */
+
