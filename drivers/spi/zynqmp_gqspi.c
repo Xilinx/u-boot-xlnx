4ffff5d4aa6e7105d9cd960edac4c070c53c8914
Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
2019-12-12 08:15:47 -0700

    zynqmp: qspi: Set multi-die flag based on SPLIT_READ config
    Set multi-die flag based on SPI_FLASH_SPLIT_READ config.
    This is required for cross die reads for the flash devices
    with multiple die's inside single flash.
    
    Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- drivers/spi/zynqmp_gqspi.c
+++ drivers/spi/zynqmp_gqspi.c
@@ -375,6 +375,18 @@ static int zynqmp_qspi_set_speed(struct udevice *bus, uint speed)
 	return 0;
 }
 
+static int zynqmp_qspi_child_pre_probe(struct udevice *bus)
+{
+	struct spi_slave *slave = dev_get_parent_priv(bus);
+
+#ifdef CONFIG_SPI_FLASH_SPLIT_READ
+	slave->multi_die = 1;
+#endif
+	slave->bytemode = SPI_4BYTE_MODE;
+
+	return 0;
+}
+
 static int zynqmp_qspi_probe(struct udevice *bus)
 {
 	struct zynqmp_qspi_plat *plat = dev_get_plat(bus);
@@ -736,4 +748,5 @@ U_BOOT_DRIVER(zynqmp_qspi) = {
 	.plat_auto	= sizeof(struct zynqmp_qspi_plat),
 	.priv_auto	= sizeof(struct zynqmp_qspi_priv),
 	.probe  = zynqmp_qspi_probe,
+	.child_pre_probe = zynqmp_qspi_child_pre_probe,
 };
