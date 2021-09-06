c6fde3aeb2c0e9a4e4bb7edc35451baba578ff09
Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
2021-09-05 23:17:32 -0600

    spi: zynqmp_gqspi: Fix write issue at low frequencies
    With automode and current implementation we are seeing write issues at
    low frequencies below 15Mhz. Make below changes to fix the issue.
    
    1. Switch genfifo start mode to manual start mode from auto start
    2. Remove dummy genfifo entry in zynqmp_qspi_chipselect() which was
       incorrectly added in the past
    3. Enable and poll for Gen_FIFO_Empty interrupt status in place of
       Gen_FIFO_not_full. Since manual mode is enabled write to genfifo first
       and then wait for the Gen_FIFO_Empty interrupt status.
    4. Enable and poll for TX_FIFO_Empty after Tx data is filled in FIFO in
       zynqmp_qspi_fill_tx_fifo(). Remove genfifo manual start before txfifo
       fill.
    
    Note: Adding this patch in a state which was intensively tested. There is
    likely no need to enable interrupts. But this chagne will be done in a
    separate patch.
    
    Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- drivers/spi/zynqmp_gqspi.c
+++ drivers/spi/zynqmp_gqspi.c
@@ -317,7 +317,6 @@ static void zynqmp_qspi_fill_gen_fifo(struct zynqmp_qspi_priv *priv,
 				GQSPI_TIMEOUT, 1);
 	if (ret)
 		printf("%s Timeout\n", __func__);
-
 }
 
 static void zynqmp_qspi_chipselect(struct zynqmp_qspi_priv *priv, int is_on)
@@ -519,7 +518,7 @@ static int zynqmp_qspi_set_mode(struct udevice *bus, uint mode)
 
 static int zynqmp_qspi_fill_tx_fifo(struct zynqmp_qspi_priv *priv, u32 size)
 {
-	u32 data;
+	u32 data, ier;
 	int ret = 0;
 	struct zynqmp_qspi_regs *regs = priv->regs;
 	u32 *buf = (u32 *)priv->tx_buf;
@@ -527,6 +526,11 @@ static int zynqmp_qspi_fill_tx_fifo(struct zynqmp_qspi_priv *priv, u32 size)
 
 	debug("TxFIFO: 0x%x, size: 0x%x\n", readl(&regs->isr),
 	      size);
+
+	/* Enable interrupts */
+	ier = readl(&regs->ier);
+	ier |= GQSPI_IXR_ALL_MASK | GQSPI_IXR_TXFIFOEMPTY_MASK;
+	writel(ier, &regs->ier);
 
 	while (size) {
 		ret = wait_for_bit_le32(&regs->isr, GQSPI_IXR_TXNFULL_MASK, 1,
