62f67599c695d40bd904083922d2b079488bf722
Michal Simek <michal.simek@xilinx.com>
2021-10-21 16:54:17 +0200

    usb: dwc3: Add support for usb3-phy PHY configuration
    When usb3-phy label is found, PHY driver is called and serdes line is
    initialized. This is preparation for serdes/psgtr driver to configure GT
    lines based on description in DT.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    [michal - Squashed with
    usb: dwc3: Separate phy init from phy power on
    ]
    State: pending

--- drivers/usb/dwc3/dwc3-generic.c
+++ drivers/usb/dwc3/dwc3-generic.c
@@ -395,6 +395,17 @@ static int dwc3_glue_probe(struct udevice *dev)
 	struct udevice *child = NULL;
 	int index = 0;
 	int ret;
+	struct phy phy;
+
+	ret = generic_phy_get_by_name(dev, "usb3-phy", &phy);
+	if (!ret) {
+		ret = generic_phy_init(&phy);
+		if (ret)
+			return ret;
+	} else if (ret != -ENOENT) {
+		debug("could not get phy (err %d)\n", ret);
+		return ret;
+	}
 
 	glue->regs = dev_read_addr(dev);
 
@@ -405,6 +416,12 @@ static int dwc3_glue_probe(struct udevice *dev)
 	ret = dwc3_glue_reset_init(dev, glue);
 	if (ret)
 		return ret;
+
+	if (phy.dev) {
+		ret = generic_phy_power_on(&phy);
+		if (ret)
+			return ret;
+	}
 
 	ret = device_find_first_child(dev, &child);
 	if (ret)
