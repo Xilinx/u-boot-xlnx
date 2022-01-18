5566ab96a66bbde2fd1b683c0a9d1a1e1667836b
T Karthik Reddy <t.karthik.reddy@xilinx.com>
2022-01-17 23:39:17 -0700

    usb: dwc3: Integrate usb5744 & usb2244 driver support
    Usb5744 & usb2244 are Microchip based usbhub and usb-2.0 based SD
    controller devices. Integrate these devices into dwc3 driver to bind and
    probe the respective drivers to get detected by usb controller.
    
    Signed-off-by: T Karthik Reddy <t.karthik.reddy@xilinx.com>
    State: pending

--- drivers/usb/dwc3/dwc3-generic.c
+++ drivers/usb/dwc3/dwc3-generic.c
@@ -407,6 +407,29 @@ static int dwc3_glue_bind(struct udevice *parent)
 	dr_mode = usb_get_dr_mode(dev_ofnode(parent));
 
 	ofnode_for_each_subnode(node, dev_ofnode(parent)) {
+		struct udevice *dev;
+		char *driver = NULL;
+		const char *name = ofnode_get_name(node);
+
+		if (!ofnode_device_is_compatible(node, "snps,dwc3")) {
+			if (ofnode_device_is_compatible(node,
+							"microchip,usb5744"))
+				driver = "usb5744";
+			else if (ofnode_device_is_compatible
+				 (node, "microchip,usb2244"))
+				driver = "usb2244";
+
+			ret = device_bind_driver_to_node(parent, driver, name,
+							 node, &dev);
+			if (ret) {
+				printf("Failed to bind: %s, err: %d\n",
+				       driver, ret);
+				return ret;
+			}
+		}
+	}
+
+	ofnode_for_each_subnode(node, dev_ofnode(parent)) {
 		const char *name = ofnode_get_name(node);
 		struct udevice *dev;
 		const char *driver = NULL;
@@ -417,25 +440,28 @@ static int dwc3_glue_bind(struct udevice *parent)
 		if (!dr_mode)
 			dr_mode = usb_get_dr_mode(node);
 
-		switch (dr_mode) {
-		case USB_DR_MODE_PERIPHERAL:
-		case USB_DR_MODE_OTG:
+		if (ofnode_device_is_compatible(node, "snps,dwc3")) {
+			switch (dr_mode) {
+			case USB_DR_MODE_PERIPHERAL:
+			case USB_DR_MODE_OTG:
 #if CONFIG_IS_ENABLED(DM_USB_GADGET)
-			debug("%s: dr_mode: OTG or Peripheral\n", __func__);
-			driver = "dwc3-generic-peripheral";
+				debug("%s: dr_mode: OTG or Peripheral\n",
+				      __func__);
+				driver = "dwc3-generic-peripheral";
 #endif
 			break;
 #if defined(CONFIG_SPL_USB_HOST) || !defined(CONFIG_SPL_BUILD)
-		case USB_DR_MODE_HOST:
-			debug("%s: dr_mode: HOST\n", __func__);
-			driver = "dwc3-generic-host";
+			case USB_DR_MODE_HOST:
+				debug("%s: dr_mode: HOST\n", __func__);
+				driver = "dwc3-generic-host";
 			break;
 #endif
-		default:
-			debug("%s: unsupported dr_mode\n", __func__);
-			return -ENODEV;
-		};
-
+			default:
+				debug("%s: unsupported dr_mode\n",
+				      __func__);
+				return -ENODEV;
+			};
+		}
 		if (!driver)
 			continue;
 
@@ -498,6 +524,7 @@ static int dwc3_glue_probe(struct udevice *dev)
 	struct dwc3_glue_ops *ops = (struct dwc3_glue_ops *)dev_get_driver_data(dev);
 	struct dwc3_glue_data *glue = dev_get_plat(dev);
 	struct udevice *child = NULL;
+	ofnode node;
 	int index = 0;
 	int ret;
 	struct phy phy;
@@ -528,6 +555,17 @@ static int dwc3_glue_probe(struct udevice *dev)
 		ret = generic_phy_power_on(&phy);
 		if (ret)
 			return ret;
+	}
+
+	ofnode_for_each_subnode(node, dev_ofnode(dev)) {
+		if (!ofnode_device_is_compatible(node, "snps,dwc3")) {
+			ret = uclass_get_device_by_ofnode(UCLASS_MISC, node, &child);
+			if (ret) {
+				printf("could not get device %s, err = %d\n",
+				       node.np->name, ret);
+				return ret;
+			}
+		}
 	}
 
 	ret = device_find_first_child(dev, &child);
