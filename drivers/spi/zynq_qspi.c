u-boot/u-boot

spi: zynq_qspi: Add child pre probe function
Add child pre probe function in the driver.
Pass is_dual, is_dio and tx_rx_mode flags to higher stack.
Also set multi_die flag if the split read config is set.

Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
State: pending

drivers/spi/zynq_qspi.c
@@ -264,6 +264,21 @@ static void zynq_qspi_init_hw(struct zynq_qspi_priv *priv)
 	writel(ZYNQ_QSPI_ENR_SPI_EN_MASK, &regs->enr);
 }
 
+static int zynq_qspi_child_pre_probe(struct udevice *bus)
+{
+	struct spi_slave *slave = dev_get_parent_priv(bus);
+	struct zynq_qspi_priv *priv = dev_get_priv(bus->parent);
+	struct zynq_qspi_platdata *plat = dev_get_platdata(bus->parent);
+
+	slave->option = priv->is_dual;
+	slave->dio = priv->is_dio;
+	slave->mode = plat->tx_rx_mode;
+#ifdef CONFIG_SPI_FLASH_SPLIT_READ
+	slave->multi_die = 1;
+#endif
+	return 0;
+}
+
 static int zynq_qspi_probe(struct udevice *bus)
 {
 	struct zynq_qspi_platdata *plat = dev_get_platdata(bus);
@@ -795,4 +810,5 @@ U_BOOT_DRIVER(zynq_qspi) = {
 	.platdata_auto_alloc_size = sizeof(struct zynq_qspi_platdata),
 	.priv_auto_alloc_size = sizeof(struct zynq_qspi_priv),
 	.probe  = zynq_qspi_probe,
+	.child_pre_probe = zynq_qspi_child_pre_probe,
 };

Xilinx/u-boot-xlnx

drivers/spi/zynq_qspi.c
@@ -280,6 +280,19 @@ static void zynq_qspi_init_hw(struct zynq_qspi_priv *priv)
 	writel(ZYNQ_QSPI_ENR_SPI_EN_MASK, &regs->enr);
 }
 
+static int zynq_qspi_child_pre_probe(struct udevice *bus)
+{
+	struct spi_slave *slave = dev_get_parent_priv(bus);
+	struct zynq_qspi_priv *priv = dev_get_priv(bus->parent);
+	struct zynq_qspi_plat *plat = dev_get_plat(bus->parent);
+
+	slave->option = priv->is_dual;
+	slave->dio = priv->is_dio;
+	slave->mode = plat->tx_rx_mode;
+
+	return 0;
+}
+
 static int zynq_qspi_probe(struct udevice *bus)
 {
 	struct zynq_qspi_plat *plat = dev_get_plat(bus);
@@ -909,4 +922,5 @@ U_BOOT_DRIVER(zynq_qspi) = {
 	.plat_auto	= sizeof(struct zynq_qspi_plat),
 	.priv_auto	= sizeof(struct zynq_qspi_priv),
 	.probe  = zynq_qspi_probe,
+	.child_pre_probe = zynq_qspi_child_pre_probe,
 };