2b0c37195e5cd1c8ee2100dc190d92f34860f3d8
Piyush Mehta <piyush.mehta@xilinx.com>
2022-06-16 15:50:15 +0530

    dt-bindings: reset: versal: add reset header
    Add reset header with USB reset node ID to support the USB reset
    via reset-controller for versal.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>

---
+++ include/dt-bindings/reset/xlnx-versal-resets.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *  Copyright (C) 2022 Xilinx, Inc.
+ */
+
+#ifndef _DT_BINDINGS_VERSAL_RESETS_H
+#define _DT_BINDINGS_VERSAL_RESETS_H
+
+#define PM_RST_USB_0			(0xc104036U)
+
+#endif
