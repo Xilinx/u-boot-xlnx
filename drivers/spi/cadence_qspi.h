39de37ce675b7b84abbc19109afc8352b659e7af
Tom Rini <trini@konsulko.com>
2022-07-15 02:31:25 -0600

    spi: cadence_qspi: Migrate CONFIG_CQSPI_REF_CLK to Kconfig
    This is a little tricky since SoCFPGA has code to determine this as
    runtime.  Introduce a guard variable for platforms to select if they
    have a static value to use.  Then for ARCH_SOCFPGA, call
    cm_get_qspi_controller_clk_hz() and otherwise continue the previous
    behavior.
    
    Cc: Jagan Teki <jagan@amarulasolutions.com>
    Signed-off-by: Tom Rini <trini@konsulko.com>
    Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
    State: upstream (55b3ba4c2ba4c464491604c5da63debae72aa5c6)

--- drivers/spi/cadence_qspi.h
+++ drivers/spi/cadence_qspi.h
@@ -365,6 +365,7 @@ int cadence_qspi_apb_exec_flash_cmd(void *reg_base, unsigned int reg);
 int cadence_qspi_apb_wait_for_dma_cmplt(struct cadence_spi_priv *priv);
 int cadence_spi_versal_flash_reset(struct udevice *dev);
 void cadence_qspi_apb_enable_linear_mode(bool enable);
+unsigned int cm_get_qspi_controller_clk_hz(void);
 int cadence_spi_versal_ctrl_reset(struct cadence_spi_priv *priv);
 
 #endif /* __CADENCE_QSPI_H__ */
