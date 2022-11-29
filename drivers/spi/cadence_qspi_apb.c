44c180311c2eb6f64e2d40b2284f956ae14c789c
Ashok Reddy Soma <ashok.reddy.soma@amd.com>
2022-11-28 23:39:25 -0700

    Revert "spi: cadence_qspi_apb: Enable linear mode only for Versal"
    This reverts commit 8eb801ff13c49ad0dd289c26ec9d2cc0d513e826.
    
    Define a weak function for cadence_qspi_apb_enable_linear_mode() and
    remove if (CONFIG_IS_ENABLED(ARCH_VERSAL)) wherever it is called from.
    
    Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@amd.com>
    State: upstream (248fe9f302df5f20d75a7d88b793db017262d750)

--- drivers/spi/cadence_qspi_apb.c
+++ drivers/spi/cadence_qspi_apb.c
@@ -55,6 +55,11 @@ int cadence_qspi_apb_wait_for_dma_cmplt(struct cadence_spi_priv *priv)
 	return 0;
 }
 
+__weak void cadence_qspi_apb_enable_linear_mode(bool enable)
+{
+	return;
+}
+
 void cadence_qspi_apb_controller_enable(void *reg_base)
 {
 	unsigned int reg;
@@ -781,9 +786,7 @@ int cadence_qspi_apb_read_execute(struct cadence_spi_priv *priv,
 	void *buf = op->data.buf.in;
 	size_t len = op->data.nbytes;
 
-	if (CONFIG_IS_ENABLED(ARCH_VERSAL))
-		cadence_qspi_apb_enable_linear_mode(true);
-
+	cadence_qspi_apb_enable_linear_mode(true);
 	if (priv->use_dac_mode && (from + len < priv->ahbsize)) {
 		if (len < 256 ||
 		    dma_memcpy(buf, priv->ahbbase + from, len) < 0) {
