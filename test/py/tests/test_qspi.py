ce264169b8a4123a0bc83dbaf4865eea45aa4bff
Love Kumar <love.kumar@amd.com>
2023-06-27 11:41:55 +0530

    test: py: tests: Add qspi flash lock/unlock test
    This patch adds testcase for locking and unlocking the qspi flash
    region, it covers the entire flash area for lock/unlock operation.
    
    It relies on boardenv* file which contains the supported flash part
    list, this test will run only for supported flash parts.
    
    Example of boardenv file:
    env__qspi_lock_unlock = {
        "supported_flash": "mt25qu512a, n25q00a, n25q512ax3",
    }
    
    Signed-off-by: Love Kumar <love.kumar@amd.com>

--- test/py/tests/test_qspi.py
+++ test/py/tests/test_qspi.py
@@ -14,6 +14,9 @@ Note: This test relies on boardenv_* containing configuration values to define
 qspi minimum and maximum frequnecies at which the flash part can operate on and
 these tests run at 5 different qspi frequnecy randomised values in the range.
 with out this, this test runs with only one frequnecy value that is 0.
+It also relies on configuration values for supported flashes for qspi lock and
+unlock cases. It will run qspi lock -unlock cases only for the supported flash
+parts.
 
 Example:
 env__qspi_freq = {
@@ -21,10 +24,15 @@ env__qspi_freq = {
     "max_freq": 100000000,
 }
 
+env__qspi_lock_unlock = {
+    "supported_flash": "mt25qu512a, n25q00a, n25q512ax3",
+}
+
 """
 page_size = 0
 erase_size = 0
 total_size = 0
+flash_part = ""
 
 # Find out qspi memory parameters
 def qspi_pre_commands(u_boot_console, freq):
@@ -64,6 +72,16 @@ def qspi_pre_commands(u_boot_console, freq):
 
         total_size *= 1024 * 1024
         print ('Total size is: ' + str(total_size) + " B")
+
+    m = re.search('Detected (.+?) with', output)
+    if m:
+        try:
+            global flash_part
+            flash_part = m.group(1)
+        except:
+            pytest.fail("QSPI flash part not recognized")
+
+        print('Detected qspi flash part is: ' + str(flash_part))
 
 # Find out minimum and maximum frequnecies that device can operate
 def qspi_find_freq_range(u_boot_console):
@@ -267,4 +285,214 @@ def test_qspi_erase_all(u_boot_console):
     while i < loop:
         qspi_pre_commands(u_boot_console, random.randint(min_f, max_f))
         qspi_erase_all(u_boot_console)
+        i = i + 1
+
+# Flash operations: erase/write/read
+def flash_ops(
+    u_boot_console, ops, start, size, offset=0, exp_ret=0, exp_str="", not_exp_str=""
+):
+    timeout = 1000000
+
+    if ops == "erase":
+        with u_boot_console.temporary_timeout(timeout):
+            output = u_boot_console.run_command("sf erase %x %x" % (start, size))
+    else:
+        with u_boot_console.temporary_timeout(timeout):
+            output = u_boot_console.run_command(
+                "sf %s %x %x %x" % (ops, offset, start, size)
+            )
+
+    if exp_str:
+        assert exp_str in output
+    if not_exp_str:
+        assert not_exp_str not in output
+
+    ret_code = u_boot_console.run_command("echo $?")
+    if exp_ret >= 0:
+        assert ret_code.endswith(str(exp_ret))
+
+    return output, ret_code
+
+# Unlock the flash before making it fail
+def qspi_unlock_exit(u_boot_console, addr, size):
+    u_boot_console.run_command("sf protect unlock %x %x" % (addr, size))
+    assert False, "FAIL: Flash lock is unable to protect the data!"
+
+# QSPI lock-unlock operations
+def qspi_lock_unlock(u_boot_console, lock_addr, lock_size):
+    addr = u_boot_utils.find_ram_base(u_boot_console)
+    expected_erase = "Erased: OK"
+    expected_write = "Written: OK"
+    expected_erase_errors = [
+        "Erase operation failed",
+        "Attempted to modify a protected sector",
+        "Erased: ERROR",
+        "is protected and cannot be erased",
+        "ERROR: flash area is locked",
+    ]
+    expected_write_errors = [
+        "ERROR: flash area is locked",
+        "Program operation failed",
+        "Attempted to modify a protected sector",
+        "Written: ERROR",
+    ]
+
+    # Find the protected/un-protected region
+    if lock_addr < (total_size // 2):
+        sect_num = (lock_addr + lock_size) // erase_size
+        x = 1
+        while x < sect_num:
+            x *= 2
+        prot_start = 0
+        prot_size = x * erase_size
+        unprot_start = prot_start + prot_size
+        unprot_size = total_size - unprot_start
+    else:
+        sect_num = (total_size - lock_addr) // erase_size
+        x = 1
+        while x < sect_num:
+            x *= 2
+        prot_start = total_size - (x * erase_size)
+        prot_size = total_size - prot_start
+        unprot_start = 0
+        unprot_size = prot_start
+
+    # Check erase/write operation before locking
+    flash_ops(u_boot_console, "erase", prot_start, prot_size, 0, 0, expected_erase)
+    flash_ops(u_boot_console, "write", prot_start, prot_size, addr, 0, expected_write)
+
+    # Locking the flash
+    u_boot_console.run_command("sf protect lock %x %x" % (lock_addr, lock_size))
+    output = u_boot_console.run_command("echo $?")
+    assert output.endswith("0")
+
+    # Check erase/write operation after locking
+    output, ret_code = flash_ops(u_boot_console, "erase", prot_start, prot_size, 0, -1)
+    if not any(error in output for error in expected_erase_errors) or ret_code.endswith(
+        "0"
+    ):
+        qspi_unlock_exit(u_boot_console, lock_addr, lock_size)
+
+    output, ret_code = flash_ops(u_boot_console, "write", prot_start, prot_size, addr, -1)
+    if not any(error in output for error in expected_write_errors) or ret_code.endswith(
+        "0"
+    ):
+        qspi_unlock_exit(u_boot_console, lock_addr, lock_size)
+
+    # Check locked sectors
+    sect_lock_start = random.randrange(prot_start, (prot_start + prot_size), erase_size)
+    if prot_size > erase_size:
+        sect_lock_size = random.randrange(
+            erase_size, (prot_start + prot_size - sect_lock_start), erase_size
+        )
+    else:
+        sect_lock_size = erase_size
+    sect_write_size = random.randint(1, sect_lock_size)
+
+    output, ret_code = flash_ops(
+        u_boot_console, "erase", sect_lock_start, sect_lock_size, 0, -1
+    )
+    if not any(error in output for error in expected_erase_errors) or ret_code.endswith(
+        "0"
+    ):
+        qspi_unlock_exit(u_boot_console, lock_addr, lock_size)
+
+    output, ret_code = flash_ops(
+        u_boot_console, "write", sect_lock_start, sect_write_size, addr, -1
+    )
+    if not any(error in output for error in expected_write_errors) or ret_code.endswith(
+        "0"
+    ):
+        qspi_unlock_exit(u_boot_console, lock_addr, lock_size)
+
+    # Check unlocked sectors
+    if unprot_size != 0:
+        sect_unlock_start = random.randrange(
+            unprot_start, (unprot_start + unprot_size), erase_size
+        )
+        if unprot_size > erase_size:
+            sect_unlock_size = random.randrange(
+                erase_size, (unprot_start + unprot_size - sect_unlock_start), erase_size
+            )
+        else:
+            sect_unlock_size = erase_size
+        sect_write_size = random.randint(1, sect_unlock_size)
+
+        output, ret_code = flash_ops(
+            u_boot_console, "erase", sect_unlock_start, sect_unlock_size, 0, -1
+        )
+        if expected_erase not in output or ret_code.endswith("1"):
+            qspi_unlock_exit(u_boot_console, lock_addr, lock_size)
+
+        output, ret_code = flash_ops(
+            u_boot_console, "write", sect_unlock_start, sect_write_size, addr, -1
+        )
+        if expected_write not in output or ret_code.endswith("1"):
+            qspi_unlock_exit(u_boot_console, lock_addr, lock_size)
+
+    # Unlocking the flash
+    u_boot_console.run_command("sf protect unlock %x %x" % (lock_addr, lock_size))
+    output = u_boot_console.run_command("echo $?")
+    assert output.endswith("0")
+
+    # Check erase/write operation after un-locking
+    flash_ops(u_boot_console, "erase", prot_start, prot_size, 0, 0, expected_erase)
+    flash_ops(u_boot_console, "write", prot_start, prot_size, addr, 0, expected_write)
+
+    # Check previous locked sectors
+    sect_lock_start = random.randrange(prot_start, (prot_start + prot_size), erase_size)
+    if prot_size > erase_size:
+        sect_lock_size = random.randrange(
+            erase_size, (prot_start + prot_size - sect_lock_start), erase_size
+        )
+    else:
+        sect_lock_size = erase_size
+    sect_write_size = random.randint(1, sect_lock_size)
+
+    flash_ops(
+        u_boot_console, "erase", sect_lock_start, sect_lock_size, 0, 0, expected_erase
+    )
+    flash_ops(
+        u_boot_console,
+        "write",
+        sect_lock_start,
+        sect_write_size,
+        addr,
+        0,
+        expected_write,
+    )
+
+@pytest.mark.buildconfigspec("cmd_bdi")
+@pytest.mark.buildconfigspec("cmd_sf")
+@pytest.mark.buildconfigspec("cmd_memory")
+def test_qspi_lock_unlock(u_boot_console):
+    qspi_find_freq_range(u_boot_console)
+    flashes = u_boot_console.config.env.get("env__qspi_lock_unlock", False)
+    if not flashes:
+        pytest.skip("No supported flash list for lock/unlock provided")
+
+    i = 0
+    while i < loop:
+        qspi_pre_commands(u_boot_console, random.randint(min_f, max_f))
+
+        flashes_list = flashes.get("supported_flash", None).split(",")
+        flashes_list = [x.strip() for x in flashes_list]
+        if flash_part not in flashes_list:
+            pytest.skip("Detected flash does not support lock/unlock")
+
+        # For lower half of memory
+        lock_addr = random.randint(0, (total_size // 2) - 1)
+        lock_size = random.randint(1, ((total_size // 2) - lock_addr))
+        qspi_lock_unlock(u_boot_console, lock_addr, lock_size)
+
+        # For upper half of memory
+        lock_addr = random.randint((total_size // 2), total_size - 1)
+        lock_size = random.randint(1, (total_size - lock_addr))
+        qspi_lock_unlock(u_boot_console, lock_addr, lock_size)
+
+        # For entire flash
+        lock_addr = random.randint(0, total_size - 1)
+        lock_size = random.randint(1, (total_size - lock_addr))
+        qspi_lock_unlock(u_boot_console, lock_addr, lock_size)
+
         i = i + 1
