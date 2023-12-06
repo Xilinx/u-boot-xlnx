0110b30f6c51f2898cc7a6fc19ca44ed75f8461d
T Karthik Reddy <t.karthik.reddy@xilinx.com>
2023-12-06 15:01:18 +0530

    spi: mtd: Use split reads if multi-die flag is set
    Some flash devices have multiple dies in it & has die cross over
    issue. When SPI_NOR_MULTI_DIE flag is set in flash id table use
    it to enable split reads to avoid above issue. Define SPI_NOR_MULTI_DIE
    new flag to flash id flags. Remove SPI_FLASH_SPLIT_READ config and
    related code from the zynq and zynqmp qspi drivers as it is redundant.
    
    Signed-off-by: T Karthik Reddy <t.karthik.reddy@xilinx.com>
    Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
    Signed-off-by: Tejas Bhumkar <tejas.arvind.bhumkar@amd.com>
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
    Link: https://lore.kernel.org/r/def598add87aeaa7d3f460e44c0386b19c4f8dcd.1701853668.git.tejas.arvind.bhumkar@amd.com
    State: waiting

--- drivers/mtd/spi/sf_internal.h
+++ drivers/mtd/spi/sf_internal.h
@@ -70,6 +70,7 @@ struct flash_info {
 #define SPI_NOR_OCTAL_READ	BIT(16)	/* Flash supports Octal Read */
 #define SPI_NOR_OCTAL_DTR_READ	BIT(17)	/* Flash supports Octal DTR Read */
 #define SPI_NOR_OCTAL_DTR_PP	BIT(18) /* Flash supports Octal DTR page program */
+#define SPI_NOR_MULTI_DIE	BIT(19) /* Flash has multi dies & need split reads*/
 };
 
 extern const struct flash_info spi_nor_ids[];
