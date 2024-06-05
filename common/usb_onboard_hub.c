cc288a7a92a57f069de665c6b45e687b011e78b4
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
2024-06-05 15:32:17 +0530

    usb: onboard-hub: add support for Microchip USB5744
    Add support for the Microchip USB5744 USB3.0 and USB2.0 Hub.
    The usb5744 driver trigger hub reset signal after soft reset.
    The usb5744 hub need to reset after the phy initialization,
    which toggles the gpio.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
    Link: https://lore.kernel.org/r/20240605100221.3571-4-venkatesh.abbarapu@amd.com

--- common/usb_onboard_hub.c
+++ common/usb_onboard_hub.c
@@ -88,11 +88,21 @@ static const struct onboard_hub_data usb2514_data = {
 	/* TBD */
 };
 
+static const struct onboard_hub_data usb5744_data = {
+	.reset_us = 10000,
+};
+
 static const struct udevice_id usb_onboard_hub_ids[] = {
 	/* Use generic usbVID,PID dt-bindings (usb-device.yaml) */
 	{
 		.compatible = "usb424,2514", /* USB2514B USB 2.0 */
 		.data = (ulong)&usb2514_data,
+	}, {
+		.compatible = "usb424,5744", /* USB5744 USB 3.0 */
+		.data = (ulong)&usb5744_data,
+	}, {
+		.compatible = "usb424,2744", /* USB2744 USB 2.0 */
+		.data = (ulong)&usb5744_data,
 	}
 };
 
