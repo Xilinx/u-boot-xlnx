53d082f1c3cda25c020f590d0857e4c869a0b2a2
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
2024-11-15 14:13:39 +0530

    mtd: spi-nor: Fix OSPI probe issue in stacked mode.
    During stacked mode of operations in the current implementation, the
    OSPI probe is failing. Enabling DTR mode for the Lower flash updates
    the reg_proto to 8_8_8, causing an inability to enable DDR mode for
    the Upper flash. The protocol is then updated to 1_1_1 during the
    Upper flash configuration.
    
    Additionally, DDR tuning is skipped during Lower flash configuration
    and instead begins tuning for the Upper flash, configuring controller
    registers accordingly.
    
    Signed-off-by: Tejas Bhumkar <tejas.arvind.bhumkar@amd.com>
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
    State: pending

--- drivers/mtd/spi/spi-nor-core.c
+++ drivers/mtd/spi/spi-nor-core.c
@@ -4100,6 +4100,9 @@ static int spi_nor_micron_octal_dtr_enable(struct spi_nor *nor)
 	u8 addr_width = 3;
 	int ret;
 
+	if ((nor->flags & SNOR_F_HAS_STACKED) && (nor->spi->flags & SPI_XFER_U_PAGE))
+		nor->reg_proto = SNOR_PROTO_1_1_1;
+
 	/* Set dummy cycles for Fast Read to the default of 20. */
 	ret = write_enable(nor);
 	if (ret)
@@ -4115,9 +4118,11 @@ static int spi_nor_micron_octal_dtr_enable(struct spi_nor *nor)
 	if (ret)
 		return ret;
 
-	ret = spi_nor_wait_till_ready(nor);
-	if (ret)
-		return ret;
+	if (!(nor->flags & SNOR_F_HAS_STACKED)) {
+		ret = spi_nor_wait_till_ready(nor);
+		if (ret)
+			return ret;
+	}
 
 	nor->read_dummy = 20;
 
@@ -4137,6 +4142,12 @@ static int spi_nor_micron_octal_dtr_enable(struct spi_nor *nor)
 		dev_err(nor->dev, "Failed to enable octal DTR mode\n");
 		return ret;
 	}
+
+	/* If Stacked mode is activated, skip tuning DDR for the Lower flash and
+	 * instead tune it for the Upper flash.
+	 */
+	if ((nor->flags & SNOR_F_HAS_STACKED) && !(nor->spi->flags & SPI_XFER_U_PAGE))
+		return 0;
 
 	/* Read flash ID to make sure the switch was successful. */
 	op = (struct spi_mem_op)
@@ -4208,6 +4219,9 @@ static int spi_nor_macronix_octal_dtr_enable(struct spi_nor *nor)
 	int ret;
 	u8 *buf = nor->cmd_buf;
 
+	if ((nor->flags & SNOR_F_HAS_STACKED) && (nor->spi->flags & SPI_XFER_U_PAGE))
+		nor->reg_proto = SNOR_PROTO_1_1_1;
+
 	ret = write_enable(nor);
 	if (ret)
 		return ret;
@@ -4223,9 +4237,11 @@ static int spi_nor_macronix_octal_dtr_enable(struct spi_nor *nor)
 	if (ret)
 		return ret;
 
-	ret = spi_nor_wait_till_ready(nor);
-	if (ret)
-		return ret;
+	if (!(nor->flags & SNOR_F_HAS_STACKED)) {
+		ret = spi_nor_wait_till_ready(nor);
+		if (ret)
+			return ret;
+	}
 
 	nor->read_dummy = MXIC_MAX_DC;
 	ret = write_enable(nor);
@@ -4245,6 +4261,12 @@ static int spi_nor_macronix_octal_dtr_enable(struct spi_nor *nor)
 		dev_err(nor->dev, "Failed to enable octal DTR mode\n");
 		return ret;
 	}
+
+	/* If Stacked mode is activated, skip tuning DDR for the Lower flash and
+	 * instead tune it for the Upper flash.
+	 */
+	if ((nor->flags & SNOR_F_HAS_STACKED) && !(nor->spi->flags & SPI_XFER_U_PAGE))
+		return 0;
 
 	/* Read flash ID to make sure the switch was successful. */
 	op = (struct spi_mem_op)
