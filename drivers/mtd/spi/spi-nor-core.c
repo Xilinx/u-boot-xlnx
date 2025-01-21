Xilinx/u-boot-xlnx

commit 167f544fc7641e83371df93849139d72d8a8c3e5

mtd: spi-nor: Add support for cross die read in dual flash configuration
In a dual parallel configuration, halve the read offset.
Determine whether the read offset points to the lower or
upper flash in a dual stacked configuration and set the
corresponding flags accordingly.

Include support for cases where the read involves an odd
number of bytes.

Extend support for cross-die reads in flash memory devices
that contain multiple dies within them.

Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
Signed-off-by: Michal Simek <michal.simek@xilinx.com>
Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
Link: https://lore.kernel.org/r/025b915f46b4aeffd0a224dae195de36fe68a9f7.1701853668.git.tejas.arvind.bhumkar@amd.com
State: waiting

drivers/mtd/spi/spi-nor-core.c
@@ -1578,13 +1578,11 @@ static int spi_nor_read(struct mtd_info *mtd, loff_t from, size_t len,
 			size_t *retlen, u_char *buf)
 {
 	struct spi_nor *nor = mtd_to_spi_nor(mtd);
-	loff_t offset = from;
-	u32 rem_bank_len = 0;
-	u32 stack_shift = 0;
-	size_t read_len;
-	u8 bank;
 	int ret;
+	u32 offset = from;
 	bool is_ofst_odd = false;
+	u32 bank_size, stack_shift = 0, read_len = 0, rem_bank_len = 0;
+	u8 bank, cur_bank, nxt_bank;
 
 	dev_dbg(nor->dev, "from 0x%08x, len %zd\n", (u32)from, len);
 
@@ -1622,6 +1620,40 @@ static int spi_nor_read(struct mtd_info *mtd, loff_t from, size_t len,
 				} else {
 					nor->spi->flags &= ~SPI_XFER_U_PAGE;
 				}
+			}
+		}
+
+		if (nor->addr_width == 4) {
+			/*
+			 * Some flash devices like N25Q512 have multiple dies
+			 * in it. Read operation in these devices is bounded
+			 * by its die segment. In a continuous read, across
+			 * multiple dies, when the last byte of the selected
+			 * die segment is read, the next byte read is the
+			 * first byte of the same die segment. This is Die
+			 * cross over issue. So to handle this issue, split
+			 * a read transaction, that spans across multiple
+			 * banks, into one read per bank. Bank size is 16MB
+			 * for single and dual stacked mode and 32MB for dual
+			 * parallel mode.
+			 */
+			if (nor->spi && nor->spi->multi_die) {
+				bank_size = SZ_16M;
+				if (nor->flags & SNOR_F_HAS_PARALLEL)
+					bank_size <<= 1;
+				cur_bank = offset / bank_size;
+				nxt_bank = (offset + len) / bank_size;
+				if (cur_bank != nxt_bank)
+					rem_bank_len = (bank_size *
+							(cur_bank + 1)) -
+							offset;
+				else
+					rem_bank_len = (mtd->size >>
+							stack_shift) -
+							offset;
+			} else {
+				rem_bank_len = (mtd->size >> stack_shift) -
+						offset;
 			}
 		}
 

include/spi.h
@@ -157,7 +157,7 @@ struct spi_slave {
 	unsigned int max_write_size;
 	void *memory_map;
 
-	u8 flags;
+	u32 flags;
 #define SPI_XFER_BEGIN		BIT(0)	/* Assert CS before transfer */
 #define SPI_XFER_END		BIT(1)	/* Deassert CS after transfer */
 #define SPI_XFER_ONCE		(SPI_XFER_BEGIN | SPI_XFER_END)
@@ -173,6 +173,7 @@ struct spi_slave {
 	 */
 	bool multi_cs_cap;
 	u32 bytemode;
+	bool multi_die;		/* flash with multiple dies */
 };
 
 /**

commit a69f2f47797a768e08838d0faeca7bd02934119a

mtd: spi-nor: Fix SPI read issue
If the CONFIG_SPI_FLASH_BAR is not enabled and address width
is 3-bytes then the "rem_bank_len" value is not updated anywhere
and the spi read length becomes '0', because of this, the spi
read is giving an error. To fix this, the "rem_bank_len" value
is assigned based on the mtd size and spi flash address offset.

Fixes: 167f544fc764 ("mtd: spi-nor: Add support for cross die read in dual flash configuration")
Signed-off-by: Padmarao Begari <padmarao.begari@amd.com>
Message-ID: <20250120124135.2460502-1-padmarao.begari@amd.com>

drivers/mtd/spi/spi-nor-core.c
@@ -1652,6 +1652,8 @@ static int spi_nor_read(struct mtd_info *mtd, loff_t from, size_t len,
 					rem_bank_len *= 2;
 			}
 			rem_bank_len -= from;
+		} else {
+			rem_bank_len = mtd->size - from;
 		}
 
 		if (CONFIG_IS_ENABLED(SPI_STACKED_PARALLEL)) {