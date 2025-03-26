Xilinx/u-boot-xlnx

commit 00f186e952be04a72098e3e89557e17294ea64ac

usb: onboard-hub: Update return values of regulator APIs
Don't error out if there is no vdd regulator supply, as these are
optional properties. apart from ENOENT observing return value
as ENOSYS when !DM_REGULATOR that's why cover both configurations.

Signed-off-by: Padmarao Begari <padmarao.begari@amd.com>
Message-ID: <20250325145014.374860-6-padmarao.begari@amd.com>

common/usb_onboard_hub.c
@@ -146,7 +146,7 @@ static int usb_onboard_hub_probe(struct udevice *dev)
 	int ret;
 
 	ret = device_get_supply_regulator(dev, "vdd-supply", &hub->vdd);
-	if (ret && ret != -ENOENT) {
+	if (ret && (ret != -ENOENT || ret != -ENOSYS)) {
 		dev_err(dev, "can't get vdd-supply: %d\n", ret);
 		return ret;
 	}

commit b7465caf60b9bf19d6221270ebf492cc7f81915b

usb: onboard-hub: Fix return type for regulator APIs
Apart from ENOENT observing return value as ENOSYS when
!DM_REGULATOR that's why cover both configurations.
Changed code is not working as operation should be "&&"
not "||" (ret != -ENOENT && ret != -ENOSYS).

Also fix the remove function where the regulator_set_enable_if_allowed()
function is returning an error.

Fixes: 00f186e952be ("usb: onboard-hub: Update return values of regulator APIs")
Signed-off-by: Padmarao Begari <padmarao.begari@amd.com>
Message-ID: <20250326062359.535304-1-padmarao.begari@amd.com>

common/usb_onboard_hub.c
@@ -146,7 +146,7 @@ static int usb_onboard_hub_probe(struct udevice *dev)
 	int ret;
 
 	ret = device_get_supply_regulator(dev, "vdd-supply", &hub->vdd);
-	if (ret && (ret != -ENOENT || ret != -ENOSYS)) {
+	if (ret && ret != -ENOENT && ret != -ENOSYS) {
 		dev_err(dev, "can't get vdd-supply: %d\n", ret);
 		return ret;
 	}
@@ -204,16 +204,21 @@ static int usb_onboard_hub_bind(struct udevice *dev)
 static int usb_onboard_hub_remove(struct udevice *dev)
 {
 	struct onboard_hub *hub = dev_get_priv(dev);
-	int ret;
 
 	if (hub->reset_gpio)
 		dm_gpio_free(hub->reset_gpio->dev, hub->reset_gpio);
 
-	ret = regulator_set_enable_if_allowed(hub->vdd, false);
-	if (ret)
-		dev_err(dev, "can't disable vdd-supply: %d\n", ret);
+	if (hub->vdd) {
+		int ret;
 
-	return ret;
+		ret = regulator_set_enable_if_allowed(hub->vdd, false);
+		if (ret)
+			dev_err(dev, "can't disable vdd-supply: %d\n", ret);
+
+		return ret;
+	}
+
+	return 0;
 }
 
 static const struct onboard_hub_data usb2514_data = {