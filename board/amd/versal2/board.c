Xilinx/u-boot-xlnx

commit c3ce4a5348f0c3289ecdc7f0a971435210db9896

Revert "board: amd: persist efi variables through usb sd"
Revert commit cb52646e468c ("board: amd: persist efi variables
through usb sd").

The usb hack is used to resolve the capsule update issue.
But it is not required if u-boot or yocto is enabled with
below configuration.

CONFIG_EFI_VARIABLES_PRESEED=y
CONFIG_EFI_CAPSULE_ON_DISK_EARLY=y
CONFIG_PREBOOT="usb start"

The efi variables are not storing in non-volatile memory but
storing in volatile memory only, which means they’re lost
after a reboot. predefined efi variables are available in
ubootefi.var file and it is embedded with U-Boot.

Signed-off-by: Padmarao Begari <padmarao.begari@amd.com>
Message-ID: <20250624065415.1181943-1-padmarao.begari@amd.com>

board/amd/versal2/board.c
@@ -26,7 +26,6 @@
 #include <asm/arch/sys_proto.h>
 #include <dm/device.h>
 #include <dm/uclass.h>
-#include <usb.h>
 #include <versalpl.h>
 #include <zynqmp_firmware.h>
 #include "../../xilinx/common/board.h"
@@ -363,14 +362,6 @@ int board_late_init(void)
 		if (ret)
 			return ret;
 	}
-
-#if IS_ENABLED(CONFIG_EFI_HAVE_CAPSULE_SUPPORT)
-	ret = usb_init();
-	if (!ret)
-		ret = usb_stor_scan(1);
-	if (ret)
-		printf("Error: SD over USB init failed for capsule support\n");
-#endif
 
 	return board_late_init_xilinx();
 }

commit 8c80b20563b27188091e05c378b11a2760d65040

board: amd: Add usb init in board late
The USB initialization hack was previously introduced to resolve
a capsule update issue. It was later reverted because U-Boot
started embedding the ubootefi.var file during the build process.
However, the ubootefi.var file is now sourced from the ESP,
which requires USB to be initialized before accessing EFI
variables. Therefore, USB initialization is now invoked earlier
in the boot process. This is a temporary solution(hack) to address
the EFI variable access issue.

Fixes: c3ce4a5348f0 ("Revert board: amd: persist efi variables through usb sd")
Signed-off-by: Padmarao Begari <padmarao.begari@amd.com>

board/amd/versal2/board.c
@@ -26,6 +26,7 @@
 #include <asm/arch/sys_proto.h>
 #include <dm/device.h>
 #include <dm/uclass.h>
+#include <usb.h>
 #include <versalpl.h>
 #include <zynqmp_firmware.h>
 #include "../../xilinx/common/board.h"
@@ -345,6 +346,12 @@ int board_late_init(void)
 {
 	int ret;
 	u32 multiboot;
+
+#if IS_ENABLED(CONFIG_EFI_HAVE_CAPSULE_SUPPORT)
+	ret = usb_init();
+	if (!ret)
+		ret = usb_stor_scan(1);
+#endif
 
 	if (!(gd->flags & GD_FLG_ENV_DEFAULT)) {
 		debug("Saved variables - Skipping\n");