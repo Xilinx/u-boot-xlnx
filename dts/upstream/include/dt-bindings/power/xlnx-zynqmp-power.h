eeb693c2947d678fb4c32e4cdcb38029d14c5beb
Michal Simek <michal.simek@amd.com>
2025-11-27 11:46:27 +0100

    dt-bindings: xilinx: Add deprecated message to headers
    All these files should go away that's why warn user about using them to
    avoid confusion which one are valid.
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    State: pending

--- dts/upstream/include/dt-bindings/power/xlnx-zynqmp-power.h
+++ dts/upstream/include/dt-bindings/power/xlnx-zynqmp-power.h
@@ -6,6 +6,13 @@
 #ifndef _DT_BINDINGS_ZYNQMP_POWER_H
 #define _DT_BINDINGS_ZYNQMP_POWER_H
 
+/*
+ * These bindings are deprecated, because they do not match the actual
+ * concept of bindings but rather contain pure firmware values.
+ * Instead include the header in the DTS source directory.
+ */
+#warning "These bindings are deprecated. Instead use the header in the DTS source directory."
+
 #define		PD_RPU_0	7
 #define		PD_RPU_1	8
 #define		PD_R5_0_ATCM	15
