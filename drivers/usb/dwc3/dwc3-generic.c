782da2392a5bdf4f1f7f6271d101a10cc65ff88f
Pranav Sanwal <pranav.sanwal@amd.com>
2025-12-05 15:34:59 +0530

    usb: dwc3: Add versal2 compatibility string to dwc3 glue ids
    Add mmi usb controller support which uses dwc3 and hence its
    compatible string needs to be added to dwc3 glue ids.
    USB3 DRD IP is usb3.1 gen2 controller which support following
    speed SSP (10-Gbps), SuperSpeed(5-Gbps), high-speed(480-Mbps),
    full-speed(12-Mbps) and low-speed(1.5-Mbps) operation modes.
    
    Signed-off-by: Pranav Sanwal <pranav.sanwal@amd.com>
    Message-ID: <20251205100459.3310232-1-pranav.sanwal@amd.com>

--- drivers/usb/dwc3/dwc3-generic.c
+++ drivers/usb/dwc3/dwc3-generic.c
@@ -711,6 +711,7 @@ int dwc3_glue_remove(struct udevice *dev)
 static const struct udevice_id dwc3_glue_ids[] = {
 	{ .compatible = "xlnx,zynqmp-dwc3" },
 	{ .compatible = "xlnx,versal-dwc3" },
+	{ .compatible = "xlnx,versal2-mmi-dwc3" },
 	{ .compatible = "ti,keystone-dwc3"},
 	{ .compatible = "ti,dwc3", .data = (ulong)&ti_ops },
 	{ .compatible = "ti,am437x-dwc3", .data = (ulong)&ti_ops },
